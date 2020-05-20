1d3b4ccddcf5a5a471dcc7516e78145f9b4704d0
Daniel Bartoníček <xdanik@users.noreply.github.com>
2020-05-20 13:00:16 +0200

    Hromadne prirazeni produktu k taxonum (#6)
    * Hide group actions behind dropdown
    * Bulk action - set products categories

---
+++ src/Controller/ManageProductCategoriesController.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MangoSylius\ExtendedChannelsPlugin\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use MangoSylius\ExtendedChannelsPlugin\Form\Type\BulkManageProductCategoriesType;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class ManageProductCategoriesController
+{
+	/**
+	 * @var RouterInterface
+	 */
+	private $router;
+	/**
+	 * @var FlashBagInterface
+	 */
+	private $flashBag;
+	/**
+	 * @var TranslatorInterface
+	 */
+	private $translator;
+	/**
+	 * @var ProductRepositoryInterface
+	 */
+	private $productRepository;
+
+	/** @var EngineInterface */
+	private $templatingEngine;
+
+	/**
+	 * @var EntityManagerInterface
+	 */
+	private $entityManager;
+
+	/**
+	 * @var FormFactoryInterface
+	 */
+	private $formFactory;
+
+	public function __construct(
+		TranslatorInterface $translator,
+		FlashBagInterface $flashBag,
+		RouterInterface $router,
+		ProductRepositoryInterface $productRepository,
+		EngineInterface $templatingEngine,
+		EntityManagerInterface $entityManager,
+		FormFactoryInterface $formFactory
+	) {
+		$this->router = $router;
+		$this->flashBag = $flashBag;
+		$this->translator = $translator;
+		$this->productRepository = $productRepository;
+		$this->templatingEngine = $templatingEngine;
+		$this->entityManager = $entityManager;
+		$this->formFactory = $formFactory;
+	}
+
+	public function bulkManageProductCategories(Request $request): Response
+	{
+		$productIds = array_filter(explode(',', $request->get('bulkProductsIds')));
+		assert(count($productIds) > 0);
+
+		if ($request->isMethod('POST')) {
+			foreach ($productIds as $productId) {
+				$product = $this->productRepository->find($productId);
+				assert($product instanceof ProductInterface);
+				$product->getProductTaxons()->clear();
+				$form = $this->formFactory->create(BulkManageProductCategoriesType::class, $product);
+				$form->handleRequest($request);
+			}
+			$this->entityManager->flush();
+
+			$message = $this->translator->trans('mango-sylius.admin.manage_product_categories.saved');
+			$this->flashBag->add('success', $message);
+
+			return new RedirectResponse($this->router->generate('sylius_admin_product_index'));
+		}
+
+		$mainProduct = $this->productRepository->find(current($productIds));
+		assert($mainProduct instanceof ProductInterface);
+		$mainProduct->getProductTaxons()->clear();
+		$form = $this->formFactory->create(BulkManageProductCategoriesType::class, $mainProduct);
+
+		return $this->templatingEngine->renderResponse('@MangoSyliusExtendedChannelsPlugin/ManageProductCategories/form.html.twig', [
+			'form' => $form->createView(),
+			'productIds' => implode(',', $productIds),
+			'paths' => [
+				'cancel' => $this->router->generate('sylius_admin_product_index'),
+			],
+		]);
+	}
+}
---
+++ src/Form/Type/BulkManageProductCategoriesType.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MangoSylius\ExtendedChannelsPlugin\Form\Type;
+
+use Sylius\Bundle\CoreBundle\Form\Type\Taxon\ProductTaxonAutocompleteChoiceType;
+use Sylius\Bundle\TaxonomyBundle\Form\Type\TaxonAutocompleteChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+final class BulkManageProductCategoriesType extends AbstractType
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function buildForm(
+		FormBuilderInterface $builder,
+		array $options
+	): void {
+		$builder
+			->add('mainTaxon', TaxonAutocompleteChoiceType::class, [
+				'label' => 'sylius.form.product.main_taxon',
+			])
+			->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event): void {
+				$product = $event->getData();
+				$form = $event->getForm();
+
+				$form->add('productTaxons', ProductTaxonAutocompleteChoiceType::class, [
+					'label' => 'sylius.form.product.taxons',
+					'product' => $product,
+					'multiple' => true,
+				]);
+			})
+			->add('submit', SubmitType::class, [
+				'label' => 'Submit',
+			]);
+	}
+}
